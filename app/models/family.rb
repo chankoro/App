22ab872883b20591e7fffac1e06c0ce437b714e8
youhei yamada <eagle.ff1214@icloud.com>
2021-09-03 23:52:05 +0000

    family model作成中。has_manyとbelongs_to追記。

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,5 +2,6 @@ class Family < ApplicationRecord
   #familyモデルの下にユーザーがいるのでFamily modelがUser modelの親になる。
   #dependent: :destroyでuserが削除されたときに一緒に削除されるようになる。
   
-  has_many :users, dependent: :destroy
+  has_many :user, dependent: :destroy
+  accepts_nested_attributes_for :user
 end
