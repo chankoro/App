eb900165991673f9c6aa520a7ad372103524310c
youhei yamada <eagle.ff1214@icloud.com>
2021-09-24 22:43:36 +0000

    application.htmlをいったん保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
+  #CSRF対策
+  protect_from_forgery with: :exception
   #ログインを催促する
   before_action :require_login
   #フラッシュメッセージの指定
   add_flash_types :success, :info, :warning, :danger
 
   private
+  #current_userを定義。現在ログインしているユーザーあるいはUser_idを取得してtrueになる。
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  #current_userが存在しない⇒current_userがいない
+  def logged_in?
+    !current_user.nil?
+  end
  
+ #ログインを催促する。
   def require_login
     redirect_to login_url unless current_user
   end
--- app/controllers/familyboards_controller.rb
+++ app/controllers/familyboards_controller.rb
@@ -13,10 +13,10 @@ class FamilyboardsController < ApplicationController
   #createアクション
   def create
     @familyboard = current_user.familyboards.new(familyboard_params)
+    #コメントの保存がうまくいかないため
       binding.pry
   #コメントを書き込んで保存する際の処理
     if @familyboard.save
- 
       redirect_to familyboards_path, success: '投稿に成功しました'
     else
   #失敗した際の処理
