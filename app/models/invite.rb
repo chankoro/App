0597e1bc6dad6916b1e57864f4e2967a56dfb073
youhei yamada <eagle.ff1214@icloud.com>
2021-09-27 08:42:56 +0000

    招待機能の暗号化機能作成

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,11 @@
 class Invite < ApplicationRecord
+#招待メールを送信する前にトークンを作成させる。 
+  before_create :generate_token
+
+  
+  private
+#トークン生成についてハッシュ化を実施する。
+#Time.currentで日本時間にしている。randメソッドで乱数を使用。
+  def generate_token
+    self.token = Digest::SHA1.hexdigest([self.family_id, Time.current, rand].join)
 end
