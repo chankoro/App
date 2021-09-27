a17e37f1ebe1f7fb20a6f3f851e5110f84cb03f7
youhei yamada <eagle.ff1214@icloud.com>
2021-09-27 07:33:40 +0000

    招待機能のmailer生成

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,14 @@ Rails.application.routes.draw do
   # resources :users, only: %i[new create show destroy]
   
   #家族idを作ってからのユーザーおよび家族情報
-  resources :families, only: %i[show] do
+  resources :families, only: %i[show edit] do
   resources :users, only: %i[new create show destroy], module: "families"
 end
   resources :users, only: %i[new create show destroy]
   #家事モデルのresource
   resources :houseworks
+  #家族掲示板モデル
   resources :familyboards
+  #招待機能モデル
+  resources :invites
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_015538) do
+ActiveRecord::Schema.define(version: 2021_09_27_041212) do
 
   create_table "families", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2021_09_24_015538) do
     t.boolean "friday", default: false, null: false
     t.boolean "saturday", default: false, null: false
     t.boolean "sunday", default: false, null: false
+  end
+
+  create_table "invites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "family_id"
+    t.string "email"
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
