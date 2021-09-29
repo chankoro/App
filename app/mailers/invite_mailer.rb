8f085ef2f12d84622d56ce6d33e184e27676b23e
youhei yamada <eagle.ff1214@icloud.com>
2021-09-29 04:02:18 +0000

    招待機能のメール作成中

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,2 +1,10 @@
 class InviteMailer < ApplicationMailer
+  
+  #招待メール
+  def invite_email
+    #招待者入力フォームで入力したemailを利用して送信する。
+    @inviter = invite #招待される人の情報
+    mail to:  invite.email,
+    subject: '招待メール'
+  end
 end
